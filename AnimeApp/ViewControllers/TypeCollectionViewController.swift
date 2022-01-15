0dffbdb5a9a6a783ba6ed8f58f1ca49df07e2817
Panin <panin1970@gmail.com>
2022-01-15 23:56:05 +0300

    view collection

--- AnimeApp/ViewControllers/TypeCollectionViewController.swift
+++ AnimeApp/ViewControllers/TypeCollectionViewController.swift
@@ -34,47 +34,21 @@ enum TypeAction: String, CaseIterable {
     case godzilla = "Godzilla"
 }
 
-private let reuseIdentifier = "Cell"
-
 class TypeCollectionViewController: UICollectionViewController {
+    
+    private let typeAction = TypeAction.allCases
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
     // MARK: UICollectionViewDataSource
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
+   
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        typeAction.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "viewAnime", for: indexPath)
     
         // Configure the cell
     
